f048b2125e2d87cf28d04fe2b1b89616f41f3cbb
Fabio Nascimento <agsuperpoder@gmail.com>
2023-07-21 04:47:17 -0300

    feat: addUser with use server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
 import { db } from "@/db";
 import { users } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 
 export default async function Home() {
   const allUsers = await db.select().from(users);
-  return <>{JSON.stringify(allUsers, null, 2)}</>;
+
+  async function addUser(data: FormData) {
+    "use server";
+
+    const fullName = data.get("fullName")?.toString();
+    const phone = data.get("numerPhone")?.toString();
+
+    if (!fullName || !phone) return;
+
+    await db.insert(users).values({
+      fullName,
+      phone,
+    });
+    revalidatePath("/");
+  }
+
+  return (
+    <>
+      {JSON.stringify(allUsers, null, 2)}
+      <form action={addUser}>
+        <input type="text" name="fullName" />
+        <input type="text" name="numerPhone" />
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
 }
